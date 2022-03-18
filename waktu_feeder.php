3d08ce7c45eedb6c0c627b36c5f72987c77cdb47
Agassi Putra <agassiputrakhautsar@gmail.com>
2022-03-18 16:13:10 +0700

    penambahakn data waktu

---
+++ waktu_feeder.php
@@ -0,0 +1,12 @@
+<?php 
+include "koneksi/koneksi.php";
+
+$sql = mysqli_query($conn, "SELECT * FROM waktu_feeder");
+$data = mysqli_fetch_array($sql);
+$time = $data[1];
+// $time2 = $data[1];
+// $time3 = $data['2'];
+// $time4 = $data['3'];
+
+echo $time;
+?>
